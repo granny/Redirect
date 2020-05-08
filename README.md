86ad7b4dd557a46f31e9dcd27fb85f7771e26649
Pavel <pkrok01@outlook.com>
2020-05-08 01:46:02 -0700

    http -> https

--- README.md
+++ README.md
@@ -2,36 +2,36 @@
 Terribly built, but get's the job done
 ### Paths that work (based on DiscordSRV's directory):
 
-> http://config.pkrok.me/ORG/PROJECT/BRANCH/FILE/LANG/OPTION
+> https://config.pkrok.me/ORG/PROJECT/BRANCH/FILE/LANG/OPTION
 
 `(Full Path)`
 
-> http://config.pkrok.me/BRANCH/FILE/LANG/OPTION
+> https://config.pkrok.me/BRANCH/FILE/LANG/OPTION
 
 `(Assuming ORG="DiscordSRV", PROJECT="DiscordSRV")`
 
-> http://config.pkrok.me/BRANCH/FILE/OPTION
+> https://config.pkrok.me/BRANCH/FILE/OPTION
 
 `(Assuming ORG="DiscordSRV", PROJECT="DiscordSRV", LANG="en")`
 
-> http://config.pkrok.me/FILE/OPTION
+> https://config.pkrok.me/FILE/OPTION
 
 `(Assuming ORG="DiscordSRV", PROJECT="DiscordSRV", LANG="en", BRANCH="master")`
 
 ### Examples:
 
-[http://config.pkrok.me/DiscordSRV/DiscordSRV/master/config/en/ConfigVersion](http://config.pkrok.me/DiscordSRV/DiscordSRV/master/config/en/ConfigVersion)
+[https://config.pkrok.me/DiscordSRV/DiscordSRV/master/config/en/ConfigVersion](https://config.pkrok.me/DiscordSRV/DiscordSRV/master/config/en/ConfigVersion)
 
 `(Full Path)`
 
-[http://config.pkrok.me/master/config/en/ConfigVersion](http://config.pkrok.me/master/config/en/ConfigVersion)
+[https://config.pkrok.me/master/config/en/ConfigVersion](https://config.pkrok.me/master/config/en/ConfigVersion)
 
 `(Assuming ORG="DiscordSRV", PROJECT="DiscordSRV")`
 
-[http://config.pkrok.me/master/config/ConfigVersion](http://config.pkrok.me/master/config/ConfigVersion)
+[https://config.pkrok.me/master/config/ConfigVersion](https://config.pkrok.me/master/config/ConfigVersion)
 
 `(Assuming ORG="DiscordSRV", PROJECT="DiscordSRV", LANG="en")`
 
-[http://config.pkrok.me/config/ConfigVersion](http://config.pkrok.me/config/ConfigVersion)
+[https://config.pkrok.me/config/ConfigVersion](https://config.pkrok.me/config/ConfigVersion)
 
 `(Assuming ORG="DiscordSRV", PROJECT="DiscordSRV", LANG="en", BRANCH="master")`
--- src/main/java/me/pkrok/redirect/route/IndexRoute.java
+++ src/main/java/me/pkrok/redirect/route/IndexRoute.java
@@ -26,29 +26,29 @@ public class IndexRoute implements Route {
 
     @Override
     public Object handle(Request request, Response response) throws Exception {
-        return "<h2><a href=\"https://github.com/pkrok01/Redirect\">Config Redirect</a></h2>" +
+        return "<h2><a href=\"httpss://github.com/pkrok01/Redirect\">Config Redirect</a></h2>" +
                 "<p>Terribly built, but get's the job done</p>" +
                 "<h3>Paths that work (based on DiscordSRV's directory):</h3>" +
-                "<p>http://config.pkrok.me/ORG/PROJECT/BRANCH/FILE/LANG/OPTION</p>" +
+                "<p>https://config.pkrok.me/ORG/PROJECT/BRANCH/FILE/LANG/OPTION</p>" +
                 "<pre>(Full Path)</pre>" +
-                "<p>http://config.pkrok.me/BRANCH/FILE/LANG/OPTION</p>" +
+                "<p>https://config.pkrok.me/BRANCH/FILE/LANG/OPTION</p>" +
                 "<pre>(Assuming ORG=\"DiscordSRV\", PROJECT=\"DiscordSRV\")</pre>" +
-                "<p>http://config.pkrok.me/BRANCH/FILE/OPTION</p>" +
+                "<p>https://config.pkrok.me/BRANCH/FILE/OPTION</p>" +
                 "<pre>(Assuming ORG=\"DiscordSRV\", PROJECT=\"DiscordSRV\", LANG=\"en\")</pre>" +
-                "<p>http://config.pkrok.me/FILE/OPTION</p>" +
+                "<p>https://config.pkrok.me/FILE/OPTION</p>" +
                 "<pre>(Assuming ORG=\"DiscordSRV\", PROJECT=\"DiscordSRV\", LANG=\"en\", BRANCH=\"master\")</pre>" +
                 "<h3>Examples:</h3>" +
-                "<a href=\"http://config.pkrok.me/DiscordSRV/DiscordSRV/master/config/en/ConfigVersion\">" +
-                "http://config.pkrok.me/DiscordSRV/DiscordSRV/master/config/en/ConfigVersion</a><br>" +
+                "<a href=\"https://config.pkrok.me/DiscordSRV/DiscordSRV/master/config/en/ConfigVersion\">" +
+                "https://config.pkrok.me/DiscordSRV/DiscordSRV/master/config/en/ConfigVersion</a><br>" +
                 "<pre>(Full Path)</pre>" +
-                "<a href=\"http://config.pkrok.me/master/config/en/ConfigVersion\">" +
-                "http://config.pkrok.me/master/config/en/ConfigVersion</a><br>" +
+                "<a href=\"https://config.pkrok.me/master/config/en/ConfigVersion\">" +
+                "https://config.pkrok.me/master/config/en/ConfigVersion</a><br>" +
                 "<pre>(Assuming ORG=\"DiscordSRV\", PROJECT=\"DiscordSRV\")</pre>" +
-                "<a href=\"http://config.pkrok.me/master/config/ConfigVersion\">" +
-                "http://config.pkrok.me/master/config/ConfigVersion</a><br>" +
+                "<a href=\"https://config.pkrok.me/master/config/ConfigVersion\">" +
+                "https://config.pkrok.me/master/config/ConfigVersion</a><br>" +
                 "<pre>(Assuming ORG=\"DiscordSRV\", PROJECT=\"DiscordSRV\", LANG=\"en\")</pre>" +
-                "<a href=\"http://config.pkrok.me/config/ConfigVersion\">" +
-                "http://config.pkrok.me/config/ConfigVersion</a>" +
+                "<a href=\"https://config.pkrok.me/config/ConfigVersion\">" +
+                "https://config.pkrok.me/config/ConfigVersion</a>" +
                 "<pre>(Assuming ORG=\"DiscordSRV\", PROJECT=\"DiscordSRV\", LANG=\"en\", BRANCH=\"master\")</pre>";
     }
 }
