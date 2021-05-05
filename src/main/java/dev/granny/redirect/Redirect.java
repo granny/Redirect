9a1ccb31b337bc928d480942110c944846e6a163
granny <pkrok01@outlook.com>
2021-05-04 23:10:47 -0700

    update copyright year

--- src/main/java/dev/granny/redirect/Redirect.java
+++ src/main/java/dev/granny/redirect/Redirect.java
@@ -1,6 +1,6 @@
 /*
  * Redirect - A redirect web server for DiscordSRV's wiki
- * Copyright (C) 2020 granny
+ * Copyright (C) 2021 granny
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
